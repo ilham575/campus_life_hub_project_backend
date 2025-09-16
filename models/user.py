612b4d8c0863a4e89ad92c051c3624e3ff70145f
Solahudeen <deensola02@gmail.com>
2025-09-16 20:31:19 +0700

    update firebase_uid

--- models/user.py
+++ models/user.py
@@ -5,6 +5,6 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    firebase_uid = Column(String, unique=True, nullable=False)
+    firebase_uid = Column(String, unique=True, nullable=True)
     username = Column(String, unique=True, index=True)
     hashed_password = Column(String, nullable=False)  # เพิ่ม field นี้
