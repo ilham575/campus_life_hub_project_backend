1a5eba481785c434c8f7b5394fdb503d85e5bc84
Supachai_280246 <6510110469@email.psu.ac.th>
2025-09-24 10:51:51 +0700

    เชื่อม Backend ในส่วน Timetable

--- models/timetable.py
+++ models/timetable.py
@@ -5,7 +5,7 @@ class Timetable(Base):
     __tablename__ = "timetables"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(String, index=True)   # อ้างถึงผู้ใช้
-    day = Column(String, index=True)
-    time = Column(String, index=True)
-    subject = Column(String)
+    user_id = Column(String, index=True, nullable=False)   # อ้างถึงผู้ใช้, ไม่ควรเป็น null
+    day = Column(String, index=True, nullable=False)
+    time = Column(String, index=True, nullable=False)
+    subject = Column(String, nullable=False)
